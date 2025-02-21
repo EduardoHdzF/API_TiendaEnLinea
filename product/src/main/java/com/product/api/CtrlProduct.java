d2714670df1a257c68bf0564016723d033d1e25a
unknown <eduardohf@ciencias.unam.mx>
2025-02-21 04:12:57 -0600

    adicion del endpoint category

---
+++ product/src/main/java/com/product/api/CtrlProduct.java
@@ -0,0 +1,29 @@
+package com.product.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.product.api.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/category")
+public class CtrlProduct {
+	
+	@GetMapping
+	 public List<Category> getCategories() {
+		List<Category> categories = new ArrayList<Category>();
+		categories.add(new Category(1,"Relojes","rlj"));
+		categories.add(new Category(2,"Carros","crr"));
+		categories.add(new Category(3,"Celulares","cls"));
+		
+		return categories;
+	}
+	
+	
+	
+}
