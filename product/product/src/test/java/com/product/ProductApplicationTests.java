84c4d758b061d3e07f3f5140921d8fe7704380bc
unknown <eduardohf@ciencias.unam.mx>
2025-02-20 18:56:20 -0600

    actualizacion del proyecto

--- product/product/src/test/java/com/product/ProductApplicationTests.java
+++ product/product/src/test/java/com/product/ProductApplicationTests.java
@@ -1,10 +1,10 @@
-package com.producto;
+package com.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductoApplicationTests {
+class ProductApplicationTests {
 
 	@Test
 	void contextLoads() {
