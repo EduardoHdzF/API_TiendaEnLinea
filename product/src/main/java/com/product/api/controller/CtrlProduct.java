8ae607f6b90ff9f522d02ef3dbb2712138f57eb7
EduardoHdzF <90948381+EduardoHdzF@users.noreply.github.com>
2025-03-01 01:09:24 -0600

    servicio implementado

--- product/src/main/java/com/product/api/controller/CtrlProduct.java
+++ product/src/main/java/com/product/api/controller/CtrlProduct.java
@@ -1,28 +1,27 @@
 package com.product.api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import com.product.api.*;
-import com.product.api.entity.Category;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.product.api.entity.Category;
+import com.product.api.service.SvcCategory;
 
 
 @RestController
 @RequestMapping("/category")
 public class CtrlProduct {
 	
+	@Autowired
+	SvcCategory svc;
+	
 	@GetMapping
 	 public List<Category> getCategories() {
-		List<Category> categories = new ArrayList<Category>();
-		categories.add(new Category(1,"Relojes","rlj"));
-		categories.add(new Category(2,"Carros","crr"));
-		categories.add(new Category(3,"Celulares","cls"));
 		
-		return categories;
+		return svc.getCategories();
 	}
 	
 	
