2ef9d5de296032236ef8ad7b78a29f1bcb5fc494
Javi Mancera <javier_manceraq@ciencias.unam.mx>
2025-03-05 23:56:59 -0600

    Feature: Data Base Exception and Api Response

--- product/src/main/java/com/product/api/service/SvcCategoryImp.java
+++ product/src/main/java/com/product/api/service/SvcCategoryImp.java
@@ -3,12 +3,14 @@ package com.product.api.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.product.api.entity.Category;
 import com.product.api.repository.RepoCategory;
+import com.product.exception.DBAccessException;
 
 @Service
 public class SvcCategoryImp implements SvcCategory{
@@ -17,6 +19,11 @@ public class SvcCategoryImp implements SvcCategory{
 	
 	@Override
 	public ResponseEntity<List<Category>> getCategories() {
+		try {
+			List<Category> categories = repo.getCategories();			
+		}catch (DataAccessException e) {
+			throw new DBAccessException(e);
+		}
 		return new ResponseEntity<>(repo.getCategories(), HttpStatus.OK);
 	}
 
